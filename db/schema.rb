a06c3bf4d52c36379c1e25ca44343db51cc4313f
jamesyu <zetaentranceserious@gmail.com>
2015-08-16 00:29:15 +0800

    Set up category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815160032) do
+ActiveRecord::Schema.define(version: 20150815162533) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text    "body"
