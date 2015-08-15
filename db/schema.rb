2a8c011bf04891790cf53c611e570df1ad1ed0d7
jamesyu <zetaentranceserious@gmail.com>
2015-08-15 23:08:26 +0800

    Set up user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815144712) do
+ActiveRecord::Schema.define(version: 20150815150334) do
 
   create_table "posts", force: true do |t|
     t.string "url"
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
   end
 
 end
