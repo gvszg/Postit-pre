dbd58916b0bf50803e07c3b95b345d20cdd2166e
jamesyu <zetaentranceserious@gmail.com>
2015-08-17 22:34:01 +0800

    New post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,44 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])    
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first # TODO: change once we have authentication 
+
+    if @post.save
+      flash[:notice] = "Your post was created!"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice]
+      redirect_to post_path
+    else
+      render 'new'
+    end
+  end
+
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
 end
