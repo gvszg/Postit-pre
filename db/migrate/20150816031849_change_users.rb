69a2a3663b074bb84a3f9ffed2db24f3fd5cf69b
jamesyu <zetaentranceserious@gmail.com>
2015-08-26 21:42:54 +0800

    Delete migration

--- db/migrate/20150816031849_change_users.rb
+++
@@ -1,8 +0,0 @@
-class ChangeUsers < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20150816032207_change_posts.rb
+++
@@ -1,9 +0,0 @@
-class ChangePosts < ActiveRecord::Migration
-  def change
-    change_table :posts do |t|
-      t.remove :foreign_key
-      
-      t.timestamps
-    end
-  end
-end
