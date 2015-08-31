753691e402c3066df1529e38d66f8626a52a7124
jamesyu <zetaentranceserious@gmail.com>
2015-08-31 17:19:35 +0800

    Sluggable Module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   include Voteable
+  include Sluggable
 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
@@ -8,40 +9,7 @@ class Post < ActiveRecord::Base
   
   validates :title, presence: true, length: {minimum: 5}
   validates :description, presence: true
-  validates :url, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true  
 
-  before_save :generate_slug!
-
-  def to_param
-    self.slug
-  end
-
-  def generate_slug!
-    the_slug = to_slug(self.title)
-    post = Post.find_by slug: the_slug
-    count = 2
-
-    while post && post != self
-      the_slug = append_suffix(the_slug, count)
-      post = Post.find_by slug: the_slug
-      count += 1
-    end
-
-    self.slug = the_slug.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0 # google-2 or google-3 etc
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s      
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/, "-"
-    str.downcase
-  end
+  sluggable_column :title
 end
