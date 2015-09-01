b40ffb0ce295981178e1f7c23bf1070967c9e826
jamesyu <zetaentranceserious@gmail.com>
2015-09-01 11:22:32 +0800

    Simple Admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}
 
   sluggable_column :username
+
+  def admin?
+    self.role == 'admin'
+  end
+
+  def moderator?
+    self.role == 'moderator'
+  end
 end
