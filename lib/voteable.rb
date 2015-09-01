98f7c107d0261a69a790f6894fe09df8b4531e17
jamesyu <zetaentranceserious@gmail.com>
2015-09-01 09:57:29 +0800

    Extract voteable to gem

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,23 +1,4 @@
-# Using Concerns
-module Voteable
-  extend ActiveSupport::Concern
 
-  included do
-    has_many :votes, as: :voteable
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
-
-  def total_votes
-    up_votes - down_votes
-  end
-end
 
 
 # Using normal metaprogramming
